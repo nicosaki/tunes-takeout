de0ba2d4ea5f088a50b874cf8fa2fa8a4aec9e0e
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-05-17 16:03:19 -0700

    User model for omniauth functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ActiveRecord::Base
+  validates :name, :uid, :provider, presence: true
+
+  def self.find_or_create_from_omniauth(auth_hash)
+    user = User.find_by(uid: auth_hash["uid"], provider: auth_hash["provider"])
+    if user.nil?
+      user = User.new(uid: auth_hash["uid"], provider: auth_hash["provider"], name: auth_hash["name"])
+      if user.save
+        return user
+      else
+        return nil
+      end
+    end
+  end
+
 end
