d4c34477a7594f5546b69fcf470c002092cdefa8
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-05-17 11:38:28 -0700

    incomplete but started suggestions controller

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -3,6 +3,14 @@ class SuggestionsController < ApplicationController
   def index
     #shows top 20 suggestions, ranked by total number of favorites
   end
+
+  def favorites
+    @user = #set user, and set user id
+    @favorites = ::TunesTakeoutWrapper.user_favorites(user_id) #returns array of pairing id's, FIX CALL
+  end
+
+
+
 favorites: shows all suggestions favorited by the signed-in User
 favorite: adds a suggestion into the favorite list for the signed-in User. This requires interaction with the Tunes & Takeout API.
 unfavorite: removes a suggestion from the favorite list for the signed-in User. This requires interaction with the Tunes & Takeout API.
