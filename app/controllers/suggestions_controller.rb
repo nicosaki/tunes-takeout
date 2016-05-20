8cddc20d7b614b6c5336edb5a1772878adb930e5
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-05-20 12:04:04 -0700

    favorites in controller

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -2,12 +2,38 @@
 class SuggestionsController < ApplicationController
   skip_before_action :require_login, only: :index
   def index
+    @top = TunesTakeoutWrapper.top(4)
     #shows top 20 suggestions, ranked by total number of favorites
+    @user = current_user
+    # if current_user
+    #   @favorites = TunesTakeoutWrapper.user_favorites(@user.user_id)
+    # end
   end
 
-  def favorites
+  def favorites #display user favorites
     @user = current_user
-    @favorites = TunesTakeout.user_favorites(user_id) #returns array of pairing id's, FIX CALL
+    @favorites = TunesTakeoutWrapper.user_favorites(@user.user_id) #returns array of pairing id's, FIX CALL
+  end
+
+  def favorite
+    @user = current_user
+    @suggestion = TunesTakeoutWrapper.favorite(@user.user_id, suggestion_id)
+  end
+
+  def unfavorite
+    @user = current_user
+    @suggestion = TunesTakeoutWrapper.unfavorite(@user.user_id, suggestion_id)
+  end
+
+  def search
+    @user = current_user
+    @suggestions = TunesTakeoutWrapper.search(params[:query]).suggestions
+    @suggestions.each do |suggestion_hash|
+      food = suggestion_hash["food_id"]
+      suggestion_hash["food_id"]= Food.retrieve(food)
+      suggestion_hash["music_id"] = Music.retrieve(suggestion_hash["music_id"], suggestion_hash["music_type"])
+    end
+    render :results
   end
 
 
